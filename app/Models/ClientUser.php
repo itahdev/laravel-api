8d91a71753a3314c9d0b820ba51acc70b992a3c8
thaidongtomosia <thai.dong@tomosia.com>
2022-08-03 08:23:24 +0700

    feature: meili search

--- app/Models/ClientUser.php
+++ app/Models/ClientUser.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Scout\Searchable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 /**
@@ -20,7 +21,8 @@ class ClientUser extends Authenticatable implements JWTSubject
         HasFactory,
         Notifiable,
         ClientUserRelationship,
-        SoftDeletes;
+        SoftDeletes,
+        Searchable;
 
     /**
      * The attributes that are mass assignable.
